42c7a7fa453e3d1e90adf6e95e07d35e4991cf01
Konen <colekahkonen@gmail.com>
2023-04-02 14:13:44 -0400

    photo count updates correctly

--- src/training/take_photos.py
+++ src/training/take_photos.py
@@ -8,12 +8,10 @@ This gives the user 3 seconds to get into position. Once in position,
 import cv2
 import mediapipe as mp
 import time
-import csv
-import numpy as np
 import os
 
 if __name__ == '__main__':
-    title = "Enter Name here"
+    title = "Average"
     
     mp_drawing = mp.solutions.drawing_utils
     mp_holistic = mp.solutions.holistic
@@ -27,7 +25,7 @@ if __name__ == '__main__':
     with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
         start_time = time.time()
 
-        counter = 0
+        counter = len(os.listdir(title))
 
         while cap.isOpened():
             ret, frame = cap.read()
@@ -38,25 +36,7 @@ if __name__ == '__main__':
 
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-            # # 1. Draw face landmarks
-            # mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS, 
-            #                         mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
-            #                         mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
-            #                         )
-            
-            # # 2. Right hand
-            # mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-            #                         mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
-            #                         mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
-            #                         )
-
-            # # 3. Left Hand
-            # mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-            #                         mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
-            #                         mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
-            #                         )
-
-            # 4. Pose Detections
+            # Pose Detections
             mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS, 
                                     mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
                                     mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
@@ -77,34 +57,5 @@ if __name__ == '__main__':
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
 
-            # export coordinates
-            # put this in its own file
-            try:
-                # Extract Pose landmarks
-                pose = results.pose_landmarks.landmark
-                pose_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
-                
-                # Extract Face landmarks
-                face = results.face_landmarks.landmark
-                face_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in face]).flatten())
-                
-                # Concatenate rows
-                row = pose_row+face_row
-                
-                # Append class name 
-                row.insert(0, title)
-                
-                # Export to CSV
-                with open('coords.csv', mode='a', newline='') as f:
-                    csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                    csv_writer.writerow(row) 
-            except:
-                pass
-
     cap.release()
     # cap.destroyAllWindows()
-
-
-    num_coords = len(results.pose_landmarks.landmark) 
-    # + len(results.face_landmarks.landmark)
-
